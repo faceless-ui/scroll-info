5bd704456fe3fbf3eb6a70e80191d184971f42e9
Jacob Fletcher <jacobsfletch@gmail.com>
2019-11-13 20:46:51 -0500

    exposes ScrollPositionContext

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
+export { default as ScrollPositionContext } from './ScrollPositionProvider/context';
 export { default as ScrollPositionProvider } from './ScrollPositionProvider';
 export { default as withScrollPosition } from './withScrollPosition';
