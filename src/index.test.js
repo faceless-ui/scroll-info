32887b3a2c5c91194e7b2e1fddb96a958d7287ba
Jacob Fletcher <jacobsfletch@gmail.com>
2020-02-05 20:12:39 -0500

    retrofits @trbl/utils into testing suite

--- src/index.test.js
+++ src/index.test.js
@@ -1,34 +1,11 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { mockWindowScrollEvents, mockRequestAnimationFrame } from '@trbl/utils';
 
 configure({ adapter: new Adapter() });
 
-window.scrollTo = function scrollTo(xCoord, yCoord) {
-  Object.assign(this, {
-    pageXOffset: xCoord,
-    pageYOffset: yCoord,
-  }).dispatchEvent(new this.Event('scroll'));
-};
+mockWindowScrollEvents({
+  scrollArea: [2483, 5000],
+});
 
-window.requestAnimationFrame = (callback) => {
-  const timestamp = performance.now();
-  callback(timestamp);
-};
-
-const addSettersToBody = (properties) => {
-  if (properties && properties.length > 0) {
-    properties.forEach((property) => {
-      Object.defineProperty(window.document.body, property, {
-        writable: true,
-        set property(value) {
-          this[property] = value;
-        },
-      });
-    });
-  }
-};
-
-addSettersToBody(['scrollWidth', 'scrollHeight']);
-
-window.document.body.scrollWidth = 2483;
-window.document.body.scrollHeight = 5000;
+mockRequestAnimationFrame();
