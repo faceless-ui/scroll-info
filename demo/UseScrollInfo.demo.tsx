21376883cf1d54f66ed08708c2631460a2891dd8
Jacob Fletcher <jacobsfletch@gmail.com>
2024-06-08 10:47:38 -0400

    chore: nodenext module resolution

--- demo/UseScrollInfo.demo.tsx
+++ demo/UseScrollInfo.demo.tsx
@@ -1,5 +1,5 @@
 import { useScrollInfo } from '@faceless-ui/scroll-info';
-import LogProps from './LogProps';
+import LogProps from './LogProps.js';
 
 const UseScrollInfo: React.FC = () => {
   const windowInfo = useScrollInfo();
